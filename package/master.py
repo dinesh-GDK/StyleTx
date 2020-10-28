4b5bee5c63e63b359cb679753d64d687c0bf841d
Dinesh Kumar Gnanasekaran <dinesh.gna111@gmail.com>
2020-10-28 20:25:01 +0530

    package encrypt

---
+++ package/master.py
@@ -0,0 +1,100 @@
+import os
+import argparse
+from cryptography.fernet import Fernet
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-t", "--task", type = str, help = "encrypt / decrypt / publish")
+ap.add_argument("-k", "--key", type = str, help = "key")
+ap.add_argument("-p", "--publish", type = str, help = "test / original")
+args = vars(ap.parse_args())
+
+no_encrypt = [".\\key.txt", ".\\master.py"]
+
+def create_package(args):
+    os.system("rmdir /Q /S dist")
+    os.system("rmdir /Q /S styletx.egg-info")
+    os.system("rmdir /Q /S build")
+
+    run = ""
+    if(args["publish"] == None):
+        print("Not enough arguments. -h for help")
+        exit()
+
+    elif(args["publish"] == "test"):
+        run = "twine upload --repository-url https://test.pypi.org/legacy/ dist/*"
+
+    elif(args["publish"] == "main"):
+        run = "twine upload dist/*"
+
+    else:
+        print("Invalid publish argument | test or main")
+        exit()
+    os.system("python setup.py sdist bdist_wheel")
+    os.system(run)
+
+
+def encrypt(args):
+
+    if(args["key"] == None):
+        print("Key not provided")
+        exit()
+
+    filesTo = []
+
+    for folder, _, files in os.walk("."):
+        for _file in files:
+            filesTo.append(folder + "\\" +  _file)
+
+    for fi in filesTo:
+        if fi not in no_encrypt:
+            with open(fi, 'r+') as f:
+                s = f.read().encode('utf-8')
+                f.seek(0)
+
+                key = args["key"].encode('utf-8')
+                key = Fernet(key)
+                encrypted_text = key.encrypt(s)
+
+                f.write(encrypted_text.decode("utf-8"))
+
+
+def decrypt(args):
+
+    if(args["key"] == None):
+        print("Key not provided")
+        exit()
+
+    filesTo = []
+
+    for folder, _, files in os.walk("."):
+        for _file in files:
+            filesTo.append(folder + "\\" +  _file)
+
+    for fi in filesTo:
+        if fi not in no_encrypt:
+            with open(fi, 'r+') as f:
+                s = f.read().encode('utf-8')
+                f.seek(0)
+
+                key = args["key"].encode('utf-8')
+                key = Fernet(key)
+                decrypted_text = key.decrypt(s)
+
+                with open(fi, 'w') as ff:
+                    ff.write(decrypted_text.decode("utf-8"))
+
+
+if __name__ == "__main__":
+
+    if(args["task"] == "publish"):
+        create_package(args)
+
+    elif(args["task"] == "encrypt"):
+        encrypt(args)
+
+    elif(args["task"] == "decrypt"):
+        decrypt(args)
+
+    else:
+        print("Invalid task argument| encrypt or decrypt or publish")
+        exit()
---
+++ package/styletx/__init__.py
@@ -0,0 +1,1 @@
+gAAAAABfmYYWNsgIh2dihdiUKtqw6o6Dz3OGV795q7uUAQhh0xzCTiuwbCu5LkrzIZCqBLw1_YZXwubBuED_Kza0itkOnp6fNhyEg-1iNbwVoR1ucXkJuDvp8cmP4EEGATy09l-lbgqA
